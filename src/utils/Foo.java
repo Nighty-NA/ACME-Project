5fa4b332be9ddf79f5258b51273094a611194a36
Nathan Arrowood <nathanielarrowood@gmail.com>
2022-08-26 14:16:27 -0400

    I hope this works

--- src/utils/Foo.java
+++ src/utils/Foo.java
@@ -5,11 +5,7 @@ public class Foo {
 	public static void main(String[] args) {
 		System.out.println("howdy");
 		System.out.println("hi from GitHub");
-<<<<<<< HEAD
 		System.out.println("hi 'nate1' branch, from Eclipse");
-		System.out.println("hello worlds");
-=======
->>>>>>> branch 'nate1' of https://github.com/Nighty-NA/ACME-Project.git
 	}
 
 }
